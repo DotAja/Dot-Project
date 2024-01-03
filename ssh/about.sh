619c1a36be3948a562de8221924929449fc7c3c0
DotAja <142836390+DotAja@users.noreply.github.com>
2024-01-03 17:58:52 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     DOT PROJECT        #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#  MODDED BY DOT AJA           #"
+echo -e "================================================="
+echo -e "# Thanks To                                     #"
+echo -e "================================================="
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# LamVpn                                        #"
+echo -e "# Horasss                                       #"
+echo -e "================================================="
